e02b00ff2151ab33feb1889f30acfa580c1ba893
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-01-15 12:40:30 +0100

    Default Function parameters

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,6 @@
-const pizzas = [
-  { name: 'Pepperoni', toppings: ['pepperoni'] }
-];
+function multiply(a: number, b: number = 25) {
+  return a * b;
+}
 
-const mappedPizzas = pizzas.map(pizza => pizza.name.toUpperCase());
-
-console.log(mappedPizzas);
-
-const pizza = {
-  name: 'Blazing inferno',
-  getName: () => pizza.name
-};
-
-console.log(pizza.getName());
+console.log(multiply(5));
+console.log(multiply(5, 35));
