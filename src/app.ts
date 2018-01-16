10c1003ad60ec303c5b16bcae584ab040e0c8835
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-01-16 11:49:49 +0100

    Typed Functions and Default Params

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
 let sumOrder: (price: number, quantity?: number) => number;
 
-/*
-sumOrder = (x, y) => {
-  if (y) {
-    return x * y
-  }
-  return x;
-};
-*/
+sumOrder = (x, y = 1) => x * y;
 
-sumOrder = (x, y) => y ? x * y : x;
+// sumOrder = (x, y) => y ? x * y : x;
 
 const sum = sumOrder(25);
 
