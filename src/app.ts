8fe89cfe4147e36be2ac37a379a50ea7ea3d047e
ZG038525H\edson.machado <edson.machado@grupocmc.es>
2018-01-16 11:43:36 +0100

    Functional and Optional Arguments

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,20 @@
-let sumOrder: (price: number, quantity: number) => number; 
+let sumOrder: (price: number, quantity?: number) => number;
 
-sumOrder = (x, y) => x * y;
+/*
+sumOrder = (x, y) => {
+  if (y) {
+    return x * y
+  }
+  return x;
+};
+*/
 
-const sum = sumOrder(25, 2);
+sumOrder = (x, y) => y ? x * y : x;
+
+const sum = sumOrder(25);
 
 console.log(`Total sum: ${sum}`);
+
+const sum2 = sumOrder(25, 2);
+
+console.log(`Total sum: ${sum2}`);
